442433111aa8b19f4a7c69a0d5cf3c56274edbfc
Trevor Mathiak <tmathiak@gmail.com>
2022-12-20 14:29:33 -0500

    added all tracked files

---
+++ models/book.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+  title: { type: String, required: true },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Author",
+    required: true,
+  },
+  summary: { type: String, required: true },
+  isbn: { type: String, required: true },
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
+
+// Virtual for the book's URL
+BookSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need this object
+  return "/catalog/book/" + this._id;
+});
+
+// Export model
+module.exports = mongoose.model("Book", BookSchema);
